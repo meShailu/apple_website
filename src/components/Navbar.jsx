e460128162f5b0295399096fe748e0ba84f5ff3a
meShailu <123243572+meShailu@users.noreply.github.com>
2024-06-25 16:13:16 +0200

    navbar compnent implemented

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { appleImg, bagImg, searchImg } from "../utils";
+import { navLists } from "../constants";
+
+const Navbar = () => {
+  return (
+    <header className="w-full  flex justify-between items-center py-5 sm:px-10">
+      <nav className="flex w-full screen-max-width">
+        <img src={appleImg} alt="Apple" width={14} height={18} />
+
+        <div className="flex flex-1 justify-center max-sm:hidden">
+          {["Phones", "Macbooks", "Tablets", "Hero"].map((nav) => (
+            <div
+              key={nav}
+              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all"
+            >
+              {nav}
+            </div>
+          ))}
+        </div>
+
+        <div className="flex items-baseline max-sm:justify-end max-sm:flex-1 gap-7">
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="bag" width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
