35be2edc96b105ee66c4234e27641d348b6cb694
meShailu <123243572+meShailu@users.noreply.github.com>
2024-06-27 12:28:13 +0200

    button and color container done

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,32 @@
 import { useGSAP } from "@gsap/react";
-import React from "react";
+import React, { useRef, useState } from "react";
 import gsap from "gsap";
+import ModelView from "./ModelView";
+import { yellowImg } from "../utils";
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
+import { models, sizes } from "../constants";
 
 const Modal = () => {
+  const [size, setSize] = useState("small");
+  const [model, setModal] = useState({
+    title: "iPhone15 Pro in Natural Titanium",
+    color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
+    img: yellowImg,
+  });
+  //camera control for the modal view
+  const cameraControlSmall = useRef();
+  const cameraControlLarge = useRef();
+
+  //modal
+
+  const small = useRef(new THREE.Group());
+  const large = useRef(new THREE.Group());
+  // rotation
+  const [smallRotation, setSmallRotation] = useState(0);
+  const [largeRotation, setLargeRotation] = useState(0);
+
   useGSAP(() => {
     gsap.to("#heading", {
       y: 0,
@@ -15,6 +39,75 @@ const Modal = () => {
         <h1 id="heading" className="section-heading">
           Take a closer look
         </h1>
+
+        <div className="flex flex-col items mt-5">
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+            <ModelView
+              index={1}
+              groupRef={small}
+              gsapType="view1"
+              controlRef={cameraControlSmall}
+              setRotationState={setSmallRotation}
+              item={Modal}
+              size={size}
+            />
+            <ModelView
+              index={2}
+              groupRef={large}
+              gsapType="view2"
+              controlRef={cameraControlLarge}
+              setRotationState={setLargeRotation}
+              item={Modal}
+              size={size}
+            />
+
+            <Canvas
+              className="w-full h-full"
+              style={{
+                position: "fixed",
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                overflow: "hidden",
+              }}
+              eventSource={document.getElementById("root")}
+            >
+              <View.Port />
+            </Canvas>
+          </div>
+          <div className="mx-auto w-full">
+            <p className="text-sm font-light mb-5 text-center">{model.title}</p>
+            <div className="flex-center">
+              <ul className="color-container">
+                {models.map((item, i) => (
+                  <li
+                    key={i}
+                    className="w-6 h-6 rounded-full mx-2 cursor-pointer"
+                    style={{ backgroundColor: item.color[0] }}
+                    onClick={() => setModal(item)}
+                  />
+                ))}
+              </ul>
+
+              <button className="size-btn-container">
+                {sizes.map(({ label, value }) => (
+                  <span
+                    key={label}
+                    className="size-btn"
+                    style={{
+                      backgroundColor: size === value ? "white" : "transparent",
+                      color: size === value ? "black" : "white",
+                    }}
+                    onClick={() => setSize(value)}
+                  >
+                    {label}
+                  </span>
+                ))}
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
